91df9889b4f6a9c7ca7a795d8b087f74eb82d3b9
Erwan Bernard <erwan.bernard@epitech.eu>
2021-01-31 23:42:12 +0100

    change interst zone in interest cells

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -143,31 +143,16 @@ impl Board {
     /// Calculate the active zone on the board.
     /// There are 10x10 big and we get them by iterating on the actually activate cell
     /// At the end we are adding the boarding zone and remove duplicates
-    fn get_actual_interest_zone(&self) -> HashSet<&Cell>{
+    fn get_actual_interest_cell(&self) -> HashSet<Cell>{
         self.actual.iter()
-            .map(|cell| (cell.x as usize / 10, cell.y as usize / 10))
-            .flat_map(|elem| {
-                let mut begin_y = if elem.1 != 0 { elem.1 - 1 } else { elem.1};
-                let mut begin_x = if elem.0 != 0 { elem.0 - 1 } else { elem.0 };
-
-                (begin_y..(elem.1 + 2)).flat_map(move |y| {
-                    (begin_x..(elem.0 + 2)).map( move |x| (x, y))
-                })
-            })
-            .collect::<HashSet<(usize, usize)>>().iter()
-            .map(|&elem| (elem.0 * 10, elem.1 * 10))
-            .flat_map(|(x, y)|
-                (y..(y + 10)).flat_map(move |a|
-                    (x..(x + 10)).filter_map(move |b| self.get_cell(b as i32, a as i32))
-                )
-            ).collect()
+            .flat_map(|cell| self.get_adj_cells(cell))
+            .collect()
     }
 
     /// Apply the game of life rules on the board
     /// Get 10x10 interesting zones and pass the rules on it
-    /// TODO, getting the active cells instead of zones will add a lot of complexity ???
     pub fn next(&mut self) {
-        let res = self.get_actual_interest_zone().iter()
+        let res = self.get_actual_interest_cell().iter()
             .map(|&cell| self.apply_on_pos(&cell))
             .filter(|cell| cell.is_alive())
             .collect::<Vec<Cell>>();
@@ -177,9 +162,7 @@ impl Board {
         self.history.push_front(self.actual.clone());
         self.actual = Box::new(HashSet::new());
 
-        res.iter().for_each(|cell| {
-            self.set_cell(cell.x, cell.y, cell.status);
-        });
+        res.iter().for_each(|cell| { self.set_cell(cell.x, cell.y, cell.status); });
         if self.history.len() >= 10 {
             self.history.pop_back();
         }
